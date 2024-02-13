4cc237e08d4a5e0b2a0cfc50fd308b85a98e8d4c
yunnn530 <159004847+yunnn530@users.noreply.github.com>
2024-02-13 10:09:15 +0900

    説明欄修正

--- vue-app/src/views/about.vue
+++ vue-app/src/views/about.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="content-container">
     <div class="content-title-group not-found">
-      <h2 class="title">Product Wish List</h2>
+      <h2 class="title">欲しいものリスト</h2>
       <p>
-        This project was created to help represent a fundamental app written
-        with Vue. The shopping theme is used throughout the app.
+        あなたが欲しいものリストに追加した商品をここで見ることが出来ます。
       </p>
       <br />
-      <h2 class="title">Resources</h2>
+      <h2 class="title">リソース</h2>
       <ul>
         <li>
           <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
