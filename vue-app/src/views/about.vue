b0480cede27190a49b3eec13d9407dc959cbcec9
yunnn530 <159004847+yunnn530@users.noreply.github.com>
2024-02-13 10:32:12 +0900

    修正削除

--- vue-app/src/views/about.vue
+++ vue-app/src/views/about.vue
@@ -3,7 +3,8 @@
     <div class="content-title-group not-found">
       <h2 class="title">欲しいものリスト</h2>
       <p>
-        あなたが欲しいものリストに追加した商品をここで見ることが出来ます。
+        This project was created to help represent a fundamental app written
+        with Vue. The shopping theme is used throughout the app.
       </p>
       <br />
       <h2 class="title">リソース</h2>
